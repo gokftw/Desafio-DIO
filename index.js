6000d147e06e0bb574fb3c824759b5b10a515f00
José Tiago <tiagodivi@hotmail.com>
2025-08-19 21:35:11 -0300

    Herói

---
+++ index.js
@@ -0,0 +1,26 @@
+// Variáveis
+let nomeHeroi = "Artemis";
+let xpHeroi = 7320;
+let nivel = "";
+
+// Estrutura de decisão
+if (xpHeroi < 1000) {
+    nivel = "Ferro";
+} else if (xpHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (xpHeroi <= 5000) {
+    nivel = "Prata";
+} else if (xpHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (xpHeroi <= 8000) {
+    nivel = "Platina";
+} else if (xpHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (xpHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome **${nomeHeroi}** está no nível de **${nivel}**`);
